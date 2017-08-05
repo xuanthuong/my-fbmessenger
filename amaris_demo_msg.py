0df87b17a7511e996c352f1313bde2cf89e85ef9
ThuongTran <xuanthuongtran@gmail.com>
2017-08-05 13:22:29 +0700

    fix

--- amaris_demo_msg.py
+++ amaris_demo_msg.py
@@ -9,36 +9,38 @@ Description: Create some message for Amaris
 """
 
 
-msg_welcome = "attachment" : {
-  "type": "template",
-  "payload": {
-    "template_type": "generic",
-    "elements": [
-      {
-        "title": "Welcome to Amaris",
-        "image_url": "https://amaris.com/Sitefinity/WebsiteTemplates/AmarisWebsiteTemplate/App_Themes/AmarisWebsite/imgs/logo_amaris.png",
-        "subtitle": '''Amaris is an independent Technology and Management Consulting Group 
-                      with high added-value consulting services on an international level''',
-        "default_action": {
-          "type": "web_url",
-          "url": "https://amaris.com",
-          "messenger_extensions": True,
-          "webview_height_ratio": "tall",
-          "fallback_url": "https://amaris.com"
-        },
-        "buttons":[
-          {
+msg_welcome = {
+  "attachment" : {
+    "type": "template",
+    "payload": {
+      "template_type": "generic",
+      "elements": [
+        {
+          "title": "Welcome to Amaris",
+          "image_url": "https://amaris.com/Sitefinity/WebsiteTemplates/AmarisWebsiteTemplate/App_Themes/AmarisWebsite/imgs/logo_amaris.png",
+          "subtitle": '''Amaris is an independent Technology and Management Consulting Group 
+                        with high added-value consulting services on an international level''',
+          "default_action": {
             "type": "web_url",
             "url": "https://amaris.com",
-            "title": "View Website"
-          },{
-            "type": "postback",
-            "title": "Start Chatting",
-            "payload": "DEVELOPER_DEFINED_PAYLOAD"
-          }              
-        ]      
-      }
-    ]
+            "messenger_extensions": True,
+            "webview_height_ratio": "tall",
+            "fallback_url": "https://amaris.com"
+          },
+          "buttons":[
+            {
+              "type": "web_url",
+              "url": "https://amaris.com",
+              "title": "View Website"
+            },{
+              "type": "postback",
+              "title": "Start Chatting",
+              "payload": "DEVELOPER_DEFINED_PAYLOAD"
+            }              
+          ]      
+        }
+      ]
+    }
   }}
 
 msg_quick_topics = {
@@ -61,72 +63,74 @@ msg_quick_topics = {
     }
   ]}
 
-msg_insight = "attachment" : {
-  "type":"template",
-  "payload":{
-    "template_type":"generic",
-    "elements":[
-      {
-        "title":"Discover the interview of Olivier Brourhant in Le Temps",
-        "image_url":"",
-        "subtitle":"Olivier Brourhant, Amaris CEO and co-founder, talks about Amaris’ success, his vision and reaching 3000 employees in only 10 years",
-        "default_action": {
-          "type": "web_url",
-          "url": "https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps",
-          "messenger_extensions": True,
-          "webview_height_ratio": "tall",
-          "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps"
+msg_insight = {
+  "attachment" : {
+    "type":"template",
+    "payload":{
+      "template_type":"generic",
+      "elements":[
+        {
+          "title":"Discover the interview of Olivier Brourhant in Le Temps",
+          "image_url":"",
+          "subtitle":"Olivier Brourhant, Amaris CEO and co-founder, talks about Amaris’ success, his vision and reaching 3000 employees in only 10 years",
+          "default_action": {
+            "type": "web_url",
+            "url": "https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps",
+            "messenger_extensions": True,
+            "webview_height_ratio": "tall",
+            "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps"
+          },
+          "buttons":[
+            {
+              "type":"web_url",
+              "url":"https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps",
+              "title":"Read more"
+            }
+          ]      
         },
-        "buttons":[
-          {
-            "type":"web_url",
-            "url":"https://amaris.com/en/amaris-website/news/news-detail/index/discover-the-interview-of-olivier-brourhant-in-le-temps",
-            "title":"Read more"
-          }
-        ]      
-      },
-      {
-        "title":"3000 employees in Amaris, 3000 inspiring reasons to apply",
-        "image_url":"",
-        "subtitle":"Today we are celebrating the fact that we have reached the milestone of 3000 employees",
-        "default_action": {
-          "type": "web_url",
-          "url": "https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!",
-          "messenger_extensions": True,
-          "webview_height_ratio": "tall",
-          "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!"
+        {
+          "title":"3000 employees in Amaris, 3000 inspiring reasons to apply",
+          "image_url":"",
+          "subtitle":"Today we are celebrating the fact that we have reached the milestone of 3000 employees",
+          "default_action": {
+            "type": "web_url",
+            "url": "https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!",
+            "messenger_extensions": True,
+            "webview_height_ratio": "tall",
+            "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!"
+          },
+          "buttons":[
+            {
+              "type":"web_url",
+              "url":"https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!",
+              "title":"Read more"
+            }
+          ]      
         },
-        "buttons":[
-          {
-            "type":"web_url",
-            "url":"https://amaris.com/en/amaris-website/news/news-detail/index/we-are-now-3000-inspiring-people-in-amaris-!",
-            "title":"Read more"
-          }
-        ]      
-      },
-      {
-        "title":"Our offices have been ISO27001 Certified",
-        "image_url":"",
-        "subtitle":"Another proof that we are a reliable asset for our clients, especially on Information Security.",
-        "default_action": {
-          "type": "web_url",
-          "url": "https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!",
-          "messenger_extensions": True,
-          "webview_height_ratio": "tall",
-          "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!"
-        },
-        "buttons":[
-          {
-            "type":"web_url",
-            "url":"https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!",
-            "title":"Read more"
-          }
-        ]      
-      }
-    ]
+        {
+          "title":"Our offices have been ISO27001 Certified",
+          "image_url":"",
+          "subtitle":"Another proof that we are a reliable asset for our clients, especially on Information Security.",
+          "default_action": {
+            "type": "web_url",
+            "url": "https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!",
+            "messenger_extensions": True,
+            "webview_height_ratio": "tall",
+            "fallback_url": "https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!"
+          },
+          "buttons":[
+            {
+              "type":"web_url",
+              "url":"https://amaris.com/en/amaris-website/news/news-detail/index/our-offices-have-been-iso27001-certified-!",
+              "title":"Read more"
+            }
+          ]      
+        }
+      ]
+    }
   }}
 
-msg_media = "attachment" : {
+msg_media = { "attachment" : {
   "type": "template",
   "payload": {
     "template_type": "generic",
@@ -172,9 +176,9 @@ msg_media = "attachment" : {
         ]      
       }
     ]
-  }}
+  }}}
 
-msg_expertise = "attachment" : {
+msg_expertise = {"attachment" : {
   "type": "template",
   "payload": {
     "template_type": "generic",
@@ -210,7 +214,7 @@ msg_expertise = "attachment" : {
         ]      
       }
     ]
-  }}
+  }}}
 
 def get_msg_amaris_welcome():
   return msg_welcome
